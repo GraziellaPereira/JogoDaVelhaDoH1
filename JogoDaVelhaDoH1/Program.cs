7f3d15c84070cb05030a22067c2ccf2a154a711f
graziella.pereira <graziellapereira52@gmail.com>
2025-03-24 21:28:53 -0300

    Separando em Classe

--- JogoDaVelhaDoH1/Program.cs
+++ JogoDaVelhaDoH1/Program.cs
@@ -1,138 +1,39 @@
-﻿string[,] tabuleiro = new string[3,3];
-string jogadorO = "O";
-string jogadorX = "X";
-string jogadorAtual = jogadorX;
-int linha, coluna;
-Random rnd = new Random();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-bool fimDeJogo=false;
-//fimDeJogo == false
-while (!fimDeJogo)
-{   if (jogadorAtual == jogadorX)
-    {
-        Console.WriteLine("Sua vez de jogar!");
-        Console.WriteLine("Digite a linha: ");
-        linha = Convert.ToInt32(Console.ReadLine()) - 1;
-
-        Console.WriteLine("Digite a coluna: ");
-        coluna = Convert.ToInt32(Console.ReadLine()) - 1;
-    } else
-    {
-        Console.WriteLine("Vez do computador!");
-        Console.ReadKey();
-        linha = rnd.Next(0, 3);
-        coluna = rnd.Next(0, 3);
-    }
-
-    if (tabuleiro[linha, coluna] == null)
-    {
-        tabuleiro[linha, coluna] = jogadorAtual;
-        if( 
-
-            (tabuleiro[0,0] == tabuleiro[0,1] &&
-            tabuleiro[0,1] == tabuleiro[0, 2] &&
-            tabuleiro[0, 0] != null
-            )
-            ||
-            (tabuleiro[1, 0] == tabuleiro[1, 1] &&
-            tabuleiro[1, 1] == tabuleiro[1, 2] &&
-            tabuleiro[1, 0] != null
-            )
-            ||
-            (tabuleiro[2, 0] == tabuleiro[2, 1] &&
-            tabuleiro[2, 1] == tabuleiro[2, 2] &&
-            tabuleiro[2, 0] != null
-            )
-            ||
-            (
-                tabuleiro[0, 0] == tabuleiro[1, 0] &&
-                tabuleiro[1, 0] == tabuleiro[2, 0] &&
-                tabuleiro[0, 0] != null
-            )
-              ||
-            (
-                tabuleiro[0, 1] == tabuleiro[1, 1] &&
-                tabuleiro[1, 1] == tabuleiro[2, 1] &&
-                tabuleiro[0, 1] != null
-            ) ||
-            (
-                tabuleiro[0, 2] == tabuleiro[1, 2] &&
-                tabuleiro[1, 2] == tabuleiro[2, 2] &&
-                tabuleiro[0, 2] != null
-            ) ||
-            (
-              tabuleiro[0,0] == tabuleiro[1, 1] &&
-              tabuleiro[1,1] == tabuleiro[2, 2] &&
-              tabuleiro[0,0] != null
-            ) ||
-            (
-              tabuleiro[2,0] == tabuleiro[1,1] &&
-              tabuleiro[1,1] == tabuleiro[0,2] &&
-              tabuleiro[2, 0] != null
-            )
-        )
-        {
-            Console.WriteLine("Vitória do jogador " 
-                + jogadorAtual);
-            fimDeJogo = true;
-        }
-        else
-            if
-            (
-            tabuleiro[0,0] != null &&
-            tabuleiro[0,1] != null &&
-            tabuleiro[0,2] != null &&
-            tabuleiro[1,0] != null &&
-            tabuleiro[1,1] != null &&
-            tabuleiro[1,2] != null &&
-            tabuleiro[2,0] != null &&
-            tabuleiro[2,1] != null &&
-            tabuleiro[2,2] != null
-
-            )
-        {
-            Console.WriteLine("Deu velha!");
-            fimDeJogo = true;
-        }
-
-        if (jogadorAtual == jogadorX)
-            jogadorAtual = jogadorO;
-        else
-            jogadorAtual = jogadorX;
-    }
-    else
-    {
-        if (jogadorAtual == jogadorX) {
-            Console.WriteLine("Posição está ocupada");
-        }
-
-    }
-    Console.Clear();
-    ImprimirTabuleiro();
-}
-
-
-
-
-
-Console.ReadLine();
-
-void ImprimirTabuleiro()
+namespace JogoDaVelhaDoH1;
+internal class Program
 {
-    for (int linhaTabuleiro = 0; linhaTabuleiro < 3; linhaTabuleiro++)
+    Random rnd = new Random();
+    void menuJogo()
     {
-        for(int colunaTabuleiro =  0; colunaTabuleiro < 3; colunaTabuleiro++)
+        bool executando = true;
+        Console.WriteLine("Vamos jogar o Jogo da Velha!");
+        Console.WriteLine("Qual vai ser o modo de jogo? Digite uma opção ou saia da aplicação.");
+        Console.WriteLine("1 - H x H (Humano vs Humano)");
+        Console.WriteLine("2 - H x M (Humano vs Máquina)");
+        Console.WriteLine("2 - H x M (Humano vs Máquina)");
+
+        int opcao = int.Parse(Console.ReadLine());
+        switch (opcao)
         {
-            if (tabuleiro[linhaTabuleiro, colunaTabuleiro] == null)
-                Console.Write("   ");
-            else
-                Console.Write(" " + tabuleiro[linhaTabuleiro, colunaTabuleiro] + " ");
-            if(colunaTabuleiro<2)
-                Console.Write( " | " );
-        }
-        Console.WriteLine();
-        if(linhaTabuleiro < 2)
-            Console.WriteLine("----------------");
- 
-    }
-}
+            case 1:
+                jogo.HxH;
+                break;
+
+            case 2:
+                jogo.HxM;
+                break;
+
+            case 3:
+                Console.WriteLine("Encerrando o sistema...");
+                executando = false;
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida! Digite de 1 a 3.");
+
+
